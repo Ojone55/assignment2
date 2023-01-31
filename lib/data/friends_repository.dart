4195780072103968e0120b2663ee07005f8b3ab7
blessing ojone <yakubuojone2018@gmail.com>
2023-01-31 08:51:34 -0800

    good

---
+++ lib/data/friends_repository.dart
@@ -0,0 +1,20 @@
+import 'package:assignment2/data/friend_model.dart';
+
+class FriendsRepo {
+  List<Friend> friends = [
+    Friend("James Alleh", "jamesalleh@gmail.com", "images/avatar_man.jpg", fav: true),
+    Friend("Ajayi Jumoke", "jummy@gmail.com", "images/avatar_girl.png"),
+    Friend("Saki Queen", "queen33@gmail.com", "images/avatar_girl.jpg"),
+    Friend("Fadwa Fuad", "Fadwa88@gmail.com", "images/avatar_girl.jpg",
+        fav: true),
+    Friend("Joan Okereke", "joano@gmail.com", "images/avatar_girl.png"),
+    Friend("Wisdom favour", "wizzy@gmail.com", "images/avatar_man.jpg"),
+    Friend("Agagwu Isaac", "isaacagagwu@gmail.com", "images/avatar_man.jpg", fav: true),
+    Friend("Edidiong Ishola", "Edidiong.Ishola@gmail.com", "images/avatar_girl.jpg"),
+    Friend("Anthony Ameh", "Anthony.Ameh@gmail.com", "images/avatar_man.jpg"),
+    Friend("Olikiabo AndyReal", "andyreal@gmail.com", "images/avatar_man.png"),
+    Friend("Emmilly Namuganga", "emmilly@gmail.com", "images/avatar_girl.jpg"),
+    Friend("Emmanuel Igunbor", "singlebond@gmail.com", "images/avatar_man.jpg"),
+    Friend("Adebayo Peace", "adebayopeace44@gmail.com", "images/avatar_man.png"),
+  ];
+}
